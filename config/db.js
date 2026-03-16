c1a684fc46492e755f9e2dd8538f6805a95057e5
Dnyaneshwar Tidme <dnyaneshwartidme@gmail.com>
2026-03-16 18:10:18 +0530

    Backend updated with latest changes

--- config/db.js
+++ config/db.js
@@ -1,29 +1,3 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-// MySQL connection configuration 
-const Db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'nits_academy'
-});
-
-// Checking the connection
-Db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to XAMPP MySQL: ' + err.stack);
-        return;
-    }
-    console.log('Successfully connected to XAMPP MySQL database!');
-});
-
-export default Db; // Use export default instead of module.exports
-
-// ..................alive live....................
-
 // import mysql from 'mysql2';
 // import dotenv from 'dotenv';
 
@@ -31,23 +5,49 @@ export default Db; // Use export default instead of module.exports
 
 // // MySQL connection configuration 
 // const Db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT || 11131, 
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME,
-//     ssl: {
-//         rejectUnauthorized: false 
-//     }
+//     host: process.env.DB_HOST || 'localhost',
+//     user: process.env.DB_USER || 'root',
+//     password: process.env.DB_PASS || '',
+//     database: process.env.DB_NAME || 'nits_academy'
 // });
 
 // // Checking the connection
 // Db.connect((err) => {
 //     if (err) {
-//         console.error('Error connecting to Aiven MySQL: ' + err.stack);
+//         console.error('Error connecting to XAMPP MySQL: ' + err.stack);
 //         return;
 //     }
-//     console.log('Successfully connected to Aiven MySQL database!');
+//     console.log('Successfully connected to XAMPP MySQL database!');
 // });
 
-// export default Db;
+// export default Db; // Use export default instead of module.exports
+
+// ..................alive live....................
+
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// MySQL connection configuration 
+const Db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 11131, 
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false 
+    }
+});
+
+// Checking the connection
+Db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to Aiven MySQL: ' + err.stack);
+        return;
+    }
+    console.log('Successfully connected to Aiven MySQL database!');
+});
+
+export default Db;
